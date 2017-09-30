7d458d8bbced8e86ff50171bd2110da7033cdc95
Richard Parry <richard.w.parry@gmail.com>
2017-09-30 16:14:02 +0100

    Section 1 - add fizzbuzz tests

---
+++ testingexamples/src/main/java/org/daisonp/testingexamples/FizzBuzz.java
@@ -0,0 +1,17 @@
+package org.daisonp.testingexamples;
+
+public class FizzBuzz {
+	public String run(int value) {
+		StringBuilder sb = new StringBuilder();
+		if (value % 3 == 0) {
+			sb.append("Fizz");
+		} 
+		if (value % 5 == 0) {
+			sb.append("Buzz");
+		}
+		if (sb.length() == 0) {
+			sb.append(value);
+		}
+		return sb.toString();
+	}
+}
